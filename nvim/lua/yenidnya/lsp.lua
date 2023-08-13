e643bf315d36f2cfeac6197025c03b5b83b02fb6
Batuhan Yenidunya <yenidnya@amazon.co.uk>
2023-08-13 13:26:39 +0100

    [nvim] update lsp options and icon values

--- nvim/lua/yenidnya/lsp.lua
+++ nvim/lua/yenidnya/lsp.lua
@@ -1,22 +1,17 @@
 local map = vim.keymap
 
 require('mason').setup()
-require('mason-lspconfig').setup({
-    ensure_installed = {
-        'rust_analyzer',
-        'tsserver',
-    }
-})
+require('mason-lspconfig').setup()
 
-local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-lsp_capabilities.textDocument.foldingRange = {
+capabilities.textDocument.foldingRange = {
     dynamicRegistration = false,
     lineFoldingOnly = true
 }
 
 local lsp_attach = function(_, bufnr)
-    -- Create your keybindings here...
     map.set('n', '<leader>ch', vim.lsp.buf.hover, { buffer = bufnr, desc = 'Hover' })
     map.set('n', '<leader>ctd', vim.lsp.buf.type_definition, { buffer = bufnr, desc = 'Type def' })
     map.set('n', '<leader>cn', vim.lsp.buf.rename, { buffer = bufnr, desc = 'Rename' })
@@ -26,22 +21,11 @@ local lsp_attach = function(_, bufnr)
     map.set('n', '<leader>cf', function()
         vim.lsp.buf.format { async = true }
     end, { buffer = bufnr, desc = 'Format' })
-
-    -- format on save
-    vim.api.nvim_create_autocmd('BufWritePre', {
-        buffer = bufnr,
-        callback = function()
-            vim.lsp.buf.format()
-        end
-    })
 end
 
 local lsp_config = {
-    capabilities = lsp_capabilities,
-    group = vim.api.nvim_create_augroup('LspFormatting', { clear = true }),
-    on_attach = function(_, bufnr)
-        lsp_attach(_, bufnr)
-    end
+    capabilities = capabilities,
+    on_attach = lsp_attach
 }
 
 require('mason-lspconfig').setup_handlers({
@@ -59,19 +43,4 @@ require('mason-lspconfig').setup_handlers({
             }
         }))
     end,
-    tsserver = function()
-        require('typescript').setup({
-            server = vim.tbl_extend('force', lsp_config, {
-                on_attach = function(_, bufnr)
-                    lsp_attach(_, bufnr)
-                end,
-                init_options = {
-                    preferences = {
-                        importModuleSpecifierPreference = 'project=relative',
-                        jsxAttributeCompletionStyle = 'none'
-                    }
-                }
-            })
-        })
-    end
 })
