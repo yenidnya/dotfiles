46b6c615bc943eff72acdfb48323f8885268a254
Batuhan Yenidunya <yenidnya@amazon.co.uk>
2023-09-29 14:01:22 +0100

    [nvim] update null-ls sources to daemons

--- nvim/lua/yenidnya/p-null_ls.lua
+++ nvim/lua/yenidnya/p-null_ls.lua
@@ -2,10 +2,13 @@ local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
     sources = {
-        formatting.prettier,
-        diagnostics.eslint
+        formatting.prettierd,
+        diagnostics.eslint_d,
+        code_actions.eslint_d
+
     },
 })
