f1586936698c938802b5a882d5fd1388c2433566
Batuhan Yenidunya <yenidnya@amazon.co.uk>
2023-09-29 14:03:15 +0100

    [nvim] create Cfname function

--- nvim/lua/yenidnya/functions.lua
+++ nvim/lua/yenidnya/functions.lua
@@ -4,6 +4,12 @@ vim.api.nvim_create_user_command("Cpath", function()
     vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, {})
 
+vim.api.nvim_create_user_command("Cfname", function()
+    local fname = vim.fn.expand("%:t")
+    vim.fn.setreg("+", fname)
+    vim.notify('Copied "' .. fname .. '" to the clipboard!')
+end, {})
+
 vim.api.nvim_create_user_command("ReloadConfig", function()
     for name, _ in pairs(package.loaded) do
         if name:match('^yenidnya') and not name:match('nvim-tree') then
