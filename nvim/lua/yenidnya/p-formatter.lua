1469df03afc846f40b95b133165b30c569f63ea4
Batuhan Yenidunya <yenidnya@amazon.co.uk>
2024-08-19 16:21:05 +0100

    [nvim] format p-formatter hehe

--- nvim/lua/yenidnya/p-formatter.lua
+++ nvim/lua/yenidnya/p-formatter.lua
@@ -1,44 +1,44 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-        {
-            "<leader>cf",
-            function()
-                require("conform").format({ async = true, lsp_fallback = true })
-            end,
-            mode = "",
-        },
-    },
-    -- Everything in opts will be passed to setup()
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            -- Use a sub-list to run only the first available formatter
-            json = { "prettierd" },
-            javascript = { "prettierd" },
-            javascriptreact = { "prettierd" },
-            typescript = { "prettierd" },
-            typescriptreact = { "prettierd" },
-            html = { "prettierd" },
-            yaml = { "prettierd" },
-            css = { "prettierd" },
-            toml = { "taplo" }
-
-        },
-        -- Customize formatters
-        formatters = {
-            prettierd = {
-                env = { PRETTIERD_DEFAULT_CONFIG = vim.fn.expand('~/.config/.prettierrc') }
-            },
-            taplo = {
-                env = { TAPLO_CONFIG = vim.fn.expand('~/.config/.taplo.toml') }
-            }
-        },
-    },
-    init = function()
-        -- If you want the formatexpr, here is the place to set it
-        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    end,
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>cf",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+		},
+	},
+	-- Everything in opts will be passed to setup()
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- Use a sub-list to run only the first available formatter
+			json = { "prettierd" },
+			javascript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			typescript = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			html = { "prettierd" },
+			yaml = { "prettierd" },
+			css = { "prettierd" },
+			toml = { "taplo" },
+			markdown = { "mdformat" },
+		},
+		-- Customize formatters
+		formatters = {
+			prettierd = {
+				env = { PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/.prettierrc") },
+			},
+			taplo = {
+				env = { TAPLO_CONFIG = vim.fn.expand("~/.config/.taplo.toml") },
+			},
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
 }
