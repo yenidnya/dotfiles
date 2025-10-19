50045d13451d09b3db105f5b0876166230c6da22
Batuhan Yenidunya <batuhanyndny@gmail.com>
2025-10-19 23:15:01 +0100

    fix(nvim): migrate mini.icons and selection option

--- .config/nvim/lua/yenidnya/p-cmp.lua
+++ .config/nvim/lua/yenidnya/p-cmp.lua
@@ -30,48 +30,31 @@ return {
 		},
 
 		completion = {
+			list = {
+				selection = {
+					preselect = false,
+				},
+			},
 			documentation = { auto_show = false },
 			menu = {
 				draw = {
 					components = {
 						kind_icon = {
 							text = function(ctx)
-								if vim.tbl_contains({ "Path" }, ctx.source_name) then
-									local mini_icon, _ = require("mini.icons").get(ctx.item.data.type, ctx.label)
-									if mini_icon then
-										return mini_icon .. ctx.icon_gap
-									end
-								end
-
-								local icon = require("lspkind").symbolic(ctx.kind, { mode = "symbol" })
-								return icon .. ctx.icon_gap
+								local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+								return kind_icon
 							end,
-
-							-- Optionally, use the highlight groups from mini.icons
-							-- You can also add the same function for `kind.highlight` if you want to
-							-- keep the highlight groups in sync with the icons.
+							-- (optional) use highlights from mini.icons
 							highlight = function(ctx)
-								if vim.tbl_contains({ "Path" }, ctx.source_name) then
-									local mini_icon, mini_hl =
-										require("mini.icons").get(ctx.item.data.type, ctx.label)
-									if mini_icon then
-										return mini_hl
-									end
-								end
-								return ctx.kind_hl
+								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+								return hl
 							end,
 						},
 						kind = {
-							-- Optional, use highlights from mini.icons
+							-- (optional) use highlights from mini.icons
 							highlight = function(ctx)
-								if vim.tbl_contains({ "Path" }, ctx.source_name) then
-									local mini_icon, mini_hl =
-										require("mini.icons").get(ctx.item.data.type, ctx.label)
-									if mini_icon then
-										return mini_hl
-									end
-								end
-								return ctx.kind_hl
+								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+								return hl
 							end,
 						},
 					},
